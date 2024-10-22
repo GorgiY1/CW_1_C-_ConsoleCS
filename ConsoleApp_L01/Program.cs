a5af9559122e374b13bae751b18a8b7dc0ebe885
johnny <orgeorgij@gmail.com>
2024-10-22 13:51:27 +0300

    Add project files.

---
+++ ConsoleApp_L01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_L01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#))");
+
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+
+            Console.ReadKey();
+
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+        }
+    }
+}
